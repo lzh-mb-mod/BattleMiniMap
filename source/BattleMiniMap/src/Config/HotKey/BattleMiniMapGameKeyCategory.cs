637797cbef33f941cbd7eec5d49a77ba8b68fc37
Zhenhuan Li <lizhenhuan1019@outlook.com>
2025-07-24 22:48:53 +0800

    Support multiple shortcuts

--- source/BattleMiniMap/src/Config/HotKey/BattleMiniMapGameKeyCategory.cs
+++ source/BattleMiniMap/src/Config/HotKey/BattleMiniMapGameKeyCategory.cs
@@ -3,6 +3,7 @@ using MissionLibrary.Provider;
 using MissionSharedLibrary.Config.HotKey;
 using MissionSharedLibrary.HotKey;
 using MissionSharedLibrary.Provider;
+using MissionSharedLibrary.Usage;
 using System;
 using System.Collections.Generic;
 using TaleWorlds.InputSystem;
@@ -23,28 +24,34 @@ namespace BattleMiniMap.Config.HotKey
 
         public static void RegisterGameKeyCategory()
         {
-            AGameKeyCategoryManager.Get()?.RegisterItem(CreateCategory());
+            AGameKeyCategoryManager.Get()?.RegisterGameKeyCategory(CreateCategory, CategoryId, new Version(1, 0));
         }
 
-        public static IProvider<AGameKeyCategory> CreateCategory()
+        public static AGameKeyCategory CreateCategory()
         {
-            return ProviderCreator.Create(() =>
-            {
-                var result = new GameKeyCategory(CategoryId,
-                    (int)GameKeyEnum.NumberOfGameKeyEnums, GameKeyConfig.Get());
-                result.AddGameKeySequence(new GameKeySequence((int)GameKeyEnum.ToggleMap, nameof(GameKeyEnum.ToggleMap),
-                    CategoryId, new List<InputKey>
-                    {
-                    InputKey.M
-                    }));
-                result.AddGameKeySequence(new GameKeySequence((int)GameKeyEnum.ToggleMapLongPress,
-                    nameof(GameKeyEnum.ToggleMapLongPress),
-                    CategoryId, new List<InputKey>
-                    {
-                    InputKey.LeftAlt
-                    }));
-                return result;
-            }, CategoryId, new Version(1, 0));
+            var result = new GameKeyCategory(CategoryId,
+                (int)GameKeyEnum.NumberOfGameKeyEnums, GameKeyConfig.Get());
+            result.AddGameKeySequence(new GameKeySequence((int)GameKeyEnum.ToggleMap, nameof(GameKeyEnum.ToggleMap),
+                CategoryId, new List<GameKeySequenceAlternative>()
+                {
+                    new GameKeySequenceAlternative(
+                        new List<InputKey>
+                        {
+                        InputKey.M
+                        }
+                    )
+                }));
+            result.AddGameKeySequence(new GameKeySequence((int)GameKeyEnum.ToggleMapLongPress,
+                nameof(GameKeyEnum.ToggleMapLongPress),
+                CategoryId, new List<GameKeySequenceAlternative>()
+                {
+                    new GameKeySequenceAlternative(new List<InputKey>
+                        {
+                        InputKey.LeftAlt
+                        }
+                    )
+                }));
+            return result;
         }
 
         public static IGameKeySequence GetKey(GameKeyEnum key)
--- source/BattleMiniMap/src/Config/HotKey/GameKeyConfig.cs
+++ source/BattleMiniMap/src/Config/HotKey/GameKeyConfig.cs
@@ -1,5 +1,6 @@
 ﻿using MissionSharedLibrary.Config;
 using MissionSharedLibrary.Config.HotKey;
+using System;
 using System.IO;
 
 namespace BattleMiniMap.Config.HotKey
@@ -8,5 +9,22 @@ namespace BattleMiniMap.Config.HotKey
     {
         protected override string SaveName { get; } =
             Path.Combine(ConfigPath.ConfigDir, BattleMiniMapSubModule.ModuleId, nameof(GameKeyConfig) + ".xml");
+            
+        protected static Version BinaryVersion => new Version(1, 1);
+
+        protected override void UpgradeToCurrentVersion()
+        {
+            switch (ConfigVersion)
+            {
+                default:
+                    ResetToDefault();
+                    Serialize();
+                    goto case "1.1";
+                case "1.1":
+                    break;
+            }
+
+            ConfigVersion = BinaryVersion.ToString(2);
+        }
     }
 }
