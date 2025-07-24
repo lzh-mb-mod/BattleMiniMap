df12c16017f988a5900af13dde25352c2805d989
Zhenhuan Li <lizhenhuan1019@outlook.com>
2025-07-24 23:36:08 +0800

    Update workshop description

--- source/BattleMiniMap/UpdateToSteamWorkshop.ps1
+++ source/BattleMiniMap/UpdateToSteamWorkshop.ps1
@@ -1,3 +1,3 @@
 [xml]$gamePathProps = Get-Content ".\source\library\mission-library\source\BasicSharedLibrary\GamePath.props"
 $gamePath = $gamePathProps.Project.PropertyGroup.GamePath
-Start-Process -FilePath "${gamePath}bin\Win64_Shipping_Client\TaleWorlds.MountAndBlade.SteamWorkshop.exe" -ArgumentList "source\BattleMiniMap\SteamWorkshopUpdate.xml"
+Start-Process -FilePath "${gamePath}bin\Win64_Shipping_Client\TaleWorlds.MountAndBlade.SteamWorkshop.exe" -ArgumentList "source\BattleMiniMap\SteamWorkshopUpdate-without-description.xml"
